f10732933994e678916674047b1cba40138adced
Gaurav2oo7 <145585355+Gaurav2oo7@users.noreply.github.com>
2023-09-20 20:46:52 +0530

    Update gst.py

--- gst.py
+++ gst.py
@@ -2,7 +2,7 @@ x=float(input("enter price of product in rupees with out tax:"))
 
 if(x>0):
     y=x*0.18
-    print("your income tax is ",y)
+    print("your GST Is ",y)
 
 else:
     print("your income cannot be less than or equal to zero")
